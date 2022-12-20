54775cbf09ef5f0f2bad0fd4bf10a57443ecf508
platoo12 <pattamatha12@gmail.com>
2022-12-20 15:27:55 +0700

    update_lab05

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = '777c5eecb4130cec5b38376935b8edf8a27c352d89a2516c'
  
 from app import views  # noqa
--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 import datetime
-from flask import jsonify, render_template
+from flask import (jsonify, render_template,
+                   request, url_for, flash, redirect)
+import json
 from app import app
  
 @app.route('/')
 def home():
     return "Flask says 'Hello world!'"
  
- 
+# ?------------------Lab02------------------------------------
 @app.route('/lab02')
 def index():
     return app.send_static_file('lab02_resume.html')
@@ -36,17 +38,43 @@ def lab03_home():
 def lab03_about():
     return render_template('lab03/about.html')
 
-@app.route('/lab03/comments/')
-def lab03_comments():
-    comments = ['This is the first comment.',
-                'This is the second comment.',
-                'This is the third comment.',
-                'This is the fourth comment.']
- 
-    return render_template('lab03/comments.html', comments=comments)
-
 
 # ?------------------Lab04------------------------------------
 @app.route('/lab04')
 def lab04_bootstrap():
     return app.send_static_file('lab04_bootstrap.html')
+
+
+# ?------------------Lab05------------------------------------
+@app.route('/lab03/comments/')
+def lab03_comments():
+    raw_json = read_file('data/messages.json')
+    messages = json.loads(raw_json)
+    return render_template('lab03/comments.html', comments=messages)
+
+def read_file(filename, mode="rt"):
+    with open(filename, mode, encoding='utf-8') as fin:
+        return fin.read()
+ 
+def write_file(filename, contents, mode="wt"):
+    with open(filename, mode, encoding="utf-8") as fout:
+        fout.write(contents)
+
+@app.route('/lab03/create/', methods=('GET', 'POST'))
+def lab03_create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+ 
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            raw_json = read_file('data/messages.json')
+            messages = json.loads(raw_json)
+            messages.append({'title': title, 'content': content})
+            write_file('data/messages.json', json.dumps(messages, indent=4))
+            return redirect(url_for('lab03_comments'))
+ 
+    return render_template('lab03/create.html')
