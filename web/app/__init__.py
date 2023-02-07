7f66f94bd3830461f339b0f3423619236df1dc94
platoo12 <pattamatha12@gmail.com>
2023-02-07 15:56:56 +0700

    database setup

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,10 +1,23 @@
+import os
 from flask import Flask
- 
-app = Flask(__name__, static_folder='static')
- 
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = '777c5eecb4130cec5b38376935b8edf8a27c352d89a2516c'
+from flask_sqlalchemy import SQLAlchemy
 
+
+app = Flask(__name__, static_folder='static')
+
+
+# this DEBUG config here will be overridden by FLASK_DEBUG shell environment
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'a8112ea716969327fc2a49fc8dd0e2ca9fa484033e771552'
 app.config['JSON_AS_ASCII'] = False
- 
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+
 from app import views  # noqa
--- web/gunicorn_starter.sh
+++ web/gunicorn_starter.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-gunicorn main:app -c gunicorn.config.py
+
+if [ "$FLASK_ENV" = "development" ]; then
+    echo "Creating the database tables..."
+    python3 manage.py create_db
+    python3 manage.py seed_db
+    echo "Tables created"
+fi
+
+gunicorn main:app -c "$PWD"/gunicorn.config.py
